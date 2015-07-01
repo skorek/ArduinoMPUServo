8a2f5df31b415a2f177452860025ae9159797154
Jakub Baron <jakubbaron@users.noreply.github.com>
2015-07-02 00:31:07 +0100

    PID activated
    Checking how responsive it is, for now it looks like it need something
    to filter the vibrations.

--- ArduinoMPUServo/FliightCtrl.ino
+++ ArduinoMPUServo/FliightCtrl.ino
@@ -12,23 +12,27 @@ void FlightControl() {
     int PIDpitch_val = (int)PIDpitch.Compute((float)setY + gx_aver);
     int PIDyaw_val = (int)PIDyaw.Compute((float)setZ - gz_aver);
 
-//    int m0_val = throttle + PIDroll_val + PIDpitch_val + PIDyaw_val;
-//    int m1_val = throttle - PIDroll_val + PIDpitch_val - PIDyaw_val;
-//    int m2_val = throttle + PIDroll_val - PIDpitch_val - PIDyaw_val;
-//    int m3_val = throttle - PIDroll_val - PIDpitch_val + PIDyaw_val;
-    int m0_val = throttle ;
-    int m1_val = throttle ;
-    int m2_val = throttle ;
-    int m3_val = throttle ;
-    if (m0_val >= 200 || m1_val >= 200 || m2_val >= 200 || m3_val >= 200) {
-      OnStop();
-    }
-    else {
+    int m0_val = throttle + PIDroll_val + PIDpitch_val + PIDyaw_val;
+    int m1_val = throttle - PIDroll_val + PIDpitch_val - PIDyaw_val;
+    int m2_val = throttle + PIDroll_val - PIDpitch_val - PIDyaw_val;
+    int m3_val = throttle - PIDroll_val - PIDpitch_val + PIDyaw_val;
+//    int m0_val = throttle ;
+//    int m1_val = throttle ;
+//    int m2_val = throttle ;
+//    int m3_val = throttle ;
+//    if (m0_val >= 220 || m1_val >= 220 || m2_val >= 220 || m3_val >= 220) {
+//      OnStop();
+//    }
+//    else {
+  if(m0_val>=200) m0_val = 200;
+    if(m1_val>=200) m1_val = 200;
+      if(m2_val>=200) m2_val = 200;
+        if(m3_val>=200) m3_val = 200;
       analogWrite(MOTOR0, m0_val);
       analogWrite(MOTOR1, m1_val);
       analogWrite(MOTOR2, m2_val);
       analogWrite(MOTOR3, m3_val);
-    }
+//    }
   }
   else {
     motorStop();
